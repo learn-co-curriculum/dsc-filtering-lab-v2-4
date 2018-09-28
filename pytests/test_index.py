1bb36443abcf70c3e78c9c96f6568a16c5eda5fa
tkoar <terrancekoar@gmail.com>
2018-09-28 14:52:34 -0400

    added tests

--- pytests/test_index.py
+++ pytests/test_index.py
@@ -1,5 +1,12 @@
 import pytest
 from sql_runner import SQLRunner
+from sql_runner_selects import SQLRunnerSelects
+from sql_selects import *
+
+sql_runner_select = SQLRunnerSelects()
+table_select = sql_runner_select.execute_create_file()
+table_select = sql_runner_select.execute_seed_file()
+
 
 def test_create_table():
     sql_runner = SQLRunner()
@@ -60,3 +67,32 @@ def test_delete_from():
     test_delete = deletion.execute("SELECT * FROM planets").fetchall()
 
     assert len(test_delete) == 8, "Delete Pluto!"
+
+
+def test_select_all_columns_and_rows():
+    result = [(1, 'Mercury', 'gray', 0, 0.55, 0.0), (2, 'Venus', 'yellow', 0, 0.82, 0.0), (3, 'Earth', 'blue', 1, 1.0, 0.0), (4, 'Mars', 'red', 2, 0.11, 0.0), (5, 'Jupiter', 'orange', 67, 317.9, 0.0), (6, 'Saturn', 'hazel', 62, 95.19, 1.0), (7, 'Uranus', 'light blue', 27, 14.54, 1.0), (8, 'Neptune', 'dark blue', 14, 17.15, 1.0)]
+    assert table_select.execute(select_all_columns_and_rows()).fetchall() == result
+
+def test_select_name_and_color_of_all_planets():
+    result = [('Mercury', 'gray'), ('Venus', 'yellow'), ('Earth', 'blue'), ('Mars', 'red'), ('Jupiter', 'orange'), ('Saturn', 'hazel'), ('Uranus', 'light blue'), ('Neptune', 'dark blue')]
+    assert table_select.execute(select_name_and_color_of_all_planets()).fetchall() == result
+
+def test_select_all_planets_with_mass_greater_than_one():
+    result = [(5, 'Jupiter', 'orange', 67, 317.9, 0.0), (6, 'Saturn', 'hazel', 62, 95.19, 1.0), (7, 'Uranus', 'light blue', 27, 14.54, 1.0), (8, 'Neptune', 'dark blue', 14, 17.15, 1.0)]
+    assert table_select.execute(select_all_planets_with_mass_greater_than_one()).fetchall() == result
+
+def test_select_name_and_mass_of_planets_with_mass_less_than_equal_to_one():
+    result = [('Mercury', 0.55), ('Venus', 0.82), ('Earth', 1.0), ('Mars', 0.11)]
+    assert table_select.execute(select_name_and_mass_of_planets_with_mass_less_than_equal_to_one()).fetchall() == result
+
+def test_select_name_and_color_of_planets_with_more_than_10_moons():
+    result = [('Jupiter', 'orange'), ('Saturn', 'hazel'), ('Uranus', 'light blue'), ('Neptune', 'dark blue')]
+    assert table_select.execute(select_name_and_color_of_planets_with_more_than_10_moons()).fetchall() == result
+
+def test_select_all_planets_with_moons_and_mass_less_than_one():
+    result = [(4, 'Mars', 'red', 2, 0.11, 0.0)]
+    assert table_select.execute(select_all_planets_with_moons_and_mass_less_than_one()).fetchall() == result
+
+def test_select_name_and_color_of_all_blue_planets():
+    result = [('Earth', 'blue'), ('Uranus', 'light blue'), ('Neptune', 'dark blue')]
+    assert table_select.execute(select_name_and_color_of_all_blue_planets()).fetchall() == result
